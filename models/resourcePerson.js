f79ca448565a34224a5493af0fd56fe114f8043c
VAIBHAV GARG <63347618+Vaibhav2801@users.noreply.github.com>
2023-02-22 23:10:56 +0530

    Specialization in response added

--- models/resourcePerson.js
+++ models/resourcePerson.js
@@ -56,8 +56,6 @@ module.exports = class ResourcePersonDetails {
         const db = getDB();
         const queryStmt = `UPDATE ${tableNames.RESOURCE_PERSON} SET
           person_name = @person_name,
-          email_id = @email_id,
-          mobile_no = @mobile_no,
           designation = @designation,
           specialization_id = @specialization_id,
           country = @country,
@@ -70,8 +68,6 @@ module.exports = class ResourcePersonDetails {
           return await db.request()
           .input('id',dbTypes.Int,id)
           .input('person_name', dbTypes.VarChar(30), data.name)
-          .input('email_id', dbTypes.VarChar(255), data.emailId)
-          .input('mobile_no', dbTypes.VarChar(10), data.mobNo)
           .input('designation', dbTypes.VarChar(30), data.designation)
           .input('specialization_id', dbTypes.Int, data.specialization_id)
           .input('country', dbTypes.VarChar(50), data.country)
@@ -116,7 +112,17 @@ module.exports = class ResourcePersonDetails {
 
       static async getResourcePersonbyId(id) {
         const db = getDB();
-        const queryStmt = `SELECT * FROM ${tableNames.RESOURCE_PERSON} WHERE id = @id`;
+        const queryStmt = `SELECT 
+        person_name,
+        email_id,
+        mobile_no,
+        designation,
+        country,
+        state_name,
+        ${tableNames.WORKSHOP_SPECIALIZATION}.specialization as area_specialization
+        FROM ${tableNames.RESOURCE_PERSON}
+        INNER JOIN ${tableNames.WORKSHOP_SPECIALIZATION}
+        ON ${tableNames.RESOURCE_PERSON}.specialization_id = ${tableNames.WORKSHOP_SPECIALIZATION}.id AND ${tableNames.RESOURCE_PERSON}.id = @id`;
         try {
           return await db.request()
           .input('id', dbTypes.Int, id)
@@ -146,29 +152,18 @@ module.exports = class ResourcePersonDetails {
     static async applyFiltersonResourcePerson(data,limit){
       const db = getDB();
       console.log(data)
-      let  queryStmt = `SELECT * FROM ${tableNames.RESOURCE_PERSON}`;
-
-      if(data.designation!=undefined || data.specialization_id!=undefined || data.country!=undefined || data.state_name!=undefined || data.organization_name!=undefined){
-        queryStmt+=` WHERE 1=1`;
-      }
-      // if(data.designation!=undefined){
-      //    queryStmt+=` AND designation =`+data.designation;
-      // }
-      // if(data.specialization_id!=undefined){
-      //   queryStmt+=` AND specialization_id=`+ data.specialization_id;
-      // }
-      // if(data.country!=undefined){
-      //   queryStmt+=` AND country=`+data.country;
-      // }
-      // if(data.state_name!=undefined){
-      //   queryStmt+=` AND state_name=`+data.state_name;
-      // }
-      // if(data.organization_name!=undefined){
-      //   queryStmt+=` AND organization_name=`+data.organization_name;
-      // }
-
-
-
+      let  queryStmt = `SELECT  
+      person_name,
+      email_id,
+      mobile_no,
+      designation,
+      country,
+      state_name,
+      ${tableNames.WORKSHOP_SPECIALIZATION}.specialization as area_specialization
+      FROM ${tableNames.RESOURCE_PERSON}
+      INNER JOIN ${tableNames.WORKSHOP_SPECIALIZATION}
+      ON ${tableNames.RESOURCE_PERSON}.specialization_id = ${tableNames.WORKSHOP_SPECIALIZATION}.id`;
+    
        if(data.designation!=undefined){
          queryStmt+=` AND designation =@designation`
       }
@@ -184,7 +179,6 @@ module.exports = class ResourcePersonDetails {
       if(data.organization_name!=undefined){
         queryStmt+=` AND organization_name=@organization_name`;
       }
-      // queryStmt+=` LIMIT `+limit ;
       try {
         return await db.request()
         .input('designation', dbTypes.VarChar(30), data.designation)
